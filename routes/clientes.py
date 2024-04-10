583f1f7689dac422257c1feedd8004859578f2fe
Adriel Rosa <minilord002@gmail.com>
2024-04-10 14:41:22 -0300

    <UPDATE>: Desenvolvimento de algumas rotas dos clientes, criação de fakedb para testes, e desenvolvimento da criação das paginas e dos fluxos

--- routes/clientes.py
+++ routes/clientes.py
@@ -1,4 +1,5 @@
-from  flask import Blueprint, render_template
+from  flask import Blueprint, render_template, request
+from database.cliente import CLIENTES
 
 cliente_route = Blueprint('cliente', __name__)
 
@@ -6,29 +7,47 @@ cliente_route = Blueprint('cliente', __name__)
 @cliente_route.route('/')
 def lista_clientes():
     """Renderiza a lista de clientes"""
-    pass
+    return render_template('lista_clientes.html', clientes=CLIENTES)
 
 @cliente_route.route('/', methods=['POST'])
 def inserir_cliente():
     """ Adiciona um novo cliente ao banco de dados """
-    pass
+    data = request.json
+    novo_usuario = {
+        "id": CLIENTES[len(CLIENTES)-1]['id']+1,
+        "nome": data['nome'],
+        "email": data['email']
+    }
+    CLIENTES.append(novo_usuario)
+    return render_template('item_cliente.html', cliente=novo_usuario)
 
 @cliente_route.route('/new')
 def form_cliente():
     """ Formulário para cadastrar um cliente """
-    pass
+    return render_template('form_cliente.html')
 
 @cliente_route.route('/<int:cliente_id>')
-def obter_cliente(cliente_id):
+def detalhe_cliente(cliente_id):
     """ Renderiza o cliente através do id informado em url """
-    pass
+    return render_template('detalhe_cliente.html')
 
-@cliente_route.route('/<int:cliente_id>/edit', methods=['PUT'])
+@cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
     """ Renderiza um formulário para editar os dados dos clientes """
+    cliente = None
+    for c in CLIENTES:
+        if c['id'] ==  cliente_id:
+            cliente = c
+    return render_template('form_cliente.html', cliente=cliente)
+
+@cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
+def atualizar_cliente(cliente_id):
+    """ Atualiza cliente no banco """
     pass
 
 @cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
 def delete_cliente(cliente_id):
     """ Deleta o cliente do banco de dados """
-    pass
+    global CLIENTES
+    CLIENTES = [ c for c in CLIENTES if c['id'] != cliente_id]
+    return {"deleted": "ok"}
