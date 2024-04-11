2edfd15a6fba9d3abf67a6247f79d8f909f490a0
Adriel Rosa <minilord002@gmail.com>
2024-04-11 14:50:57 -0300

    <UPDATE>: Finalização do projeto com a inserção do edit, ver e criação de um modal para visualização

--- routes/clientes.py
+++ routes/clientes.py
@@ -29,7 +29,8 @@ def form_cliente():
 @cliente_route.route('/<int:cliente_id>')
 def detalhe_cliente(cliente_id):
     """ Renderiza o cliente através do id informado em url """
-    return render_template('detalhe_cliente.html')
+    cliente = list(filter(lambda c: c['id'] == cliente_id, CLIENTES))[0]
+    return render_template('detalhe_cliente.html', cliente=cliente)
 
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
@@ -43,7 +44,15 @@ def form_edit_cliente(cliente_id):
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente(cliente_id):
     """ Atualiza cliente no banco """
-    pass
+    cliente_editado = None
+    data = request.json
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            c['nome'] = data['nome']
+            c['email'] = data['email']
+
+            cliente_editado = c
+    return render_template('item_cliente.html', cliente=cliente_editado)
 
 @cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
 def delete_cliente(cliente_id):
