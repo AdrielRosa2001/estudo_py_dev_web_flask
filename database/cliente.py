0531d73f37884bdd93b51681d2bee49f215ea19a
Adriel Rosa <minilord002@gmail.com>
2024-04-11 15:52:48 -0300

    <CORREÇÃO>: Correção de update anterior apenas eliminando alguns arquivos e linhas de código

--- database/cliente.py
+++
@@ -1,8 +0,0 @@
-CLIENTES = [
-    {"id": 1, "nome": "Adriel", "email": "adriel@teste.com"},
-    {"id": 2, "nome": "Matheus", "email": "mth08@teste.com"},
-    {"id": 3, "nome": "Joacir", "email": "joacirdosanjos@teste.com"},
-    {"id": 4, "nome": "Carmelia", "email": "carmelia1978@teste.com"},
-    {"id": 5, "nome": "Aziraph", "email": "azirapshcelso@teste.com"},
-    {"id": 6, "nome": "Andre", "email": "andretkd@teste.com"},
-]
